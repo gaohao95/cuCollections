8a11734818d70527e0c4e8c72df6eaf1969976a8
Yunsong Wang <yunsongw@nvidia.com>
2021-02-26 14:18:20 -0800

    Code formatting

--- tests/static_map/static_map_test.cu
+++ tests/static_map/static_map_test.cu
@@ -18,9 +18,9 @@
 #include <thrust/device_vector.h>
 #include <thrust/for_each.h>
 #include <algorithm>
-#include <limits>
 #include <catch2/catch.hpp>
 #include <cuco/static_map.cuh>
+#include <limits>
 
 namespace {
 namespace cg = cooperative_groups;
@@ -210,46 +210,40 @@ TEMPLATE_TEST_CASE_SIG("Unique sequence of keys",
 }
 
 template <typename MapType, int CAPACITY>
-__global__ void shared_memory_test_kernel(typename MapType::device_view const* const device_views,
-                                          typename MapType::device_view::key_type const* const insterted_keys,
-                                          typename MapType::device_view::mapped_type const* const inserted_values,
-                                          const size_t number_of_elements,
-                                          bool* const keys_exist,
-                                          bool* const keys_and_values_correct)
+__global__ void shared_memory_test_kernel(
+  typename MapType::device_view const* const device_views,
+  typename MapType::device_view::key_type const* const insterted_keys,
+  typename MapType::device_view::mapped_type const* const inserted_values,
+  const size_t number_of_elements,
+  bool* const keys_exist,
+  bool* const keys_and_values_correct)
 {
-    // Each block processes one map
-    const size_t map_id = blockIdx.x;
-    const size_t offset = map_id * number_of_elements;
+  // Each block processes one map
+  const size_t map_id = blockIdx.x;
+  const size_t offset = map_id * number_of_elements;
 
-    __shared__ typename MapType::pair_atomic_type sm_buffer[CAPACITY];
+  __shared__ typename MapType::pair_atomic_type sm_buffer[CAPACITY];
 
-    auto g = cg::this_thread_block();
-    typename MapType::device_view sm_device_view = MapType::device_view::make_copy(g,
-                                                                                   sm_buffer,
-                                                                                   device_views[map_id]);
+  auto g = cg::this_thread_block();
+  typename MapType::device_view sm_device_view =
+    MapType::device_view::make_copy(g, sm_buffer, device_views[map_id]);
 
-    for (int i = g.thread_rank(); i < number_of_elements; i += g.size())
-    {
-      auto found_pair_it = sm_device_view.find(insterted_keys[offset + i]);
+  for (int i = g.thread_rank(); i < number_of_elements; i += g.size()) {
+    auto found_pair_it = sm_device_view.find(insterted_keys[offset + i]);
 
-      if (found_pair_it != sm_device_view.end())
-      {
-        keys_exist[offset + i] = true;
-        if (found_pair_it->first == insterted_keys[offset + i] and found_pair_it->second == inserted_values[offset + i])
-        {
-          keys_and_values_correct[offset + i] = true;
-        }
-        else
-        {
-          keys_and_values_correct[offset + i] = false;
-        }
-      }
-      else
-      {
-        keys_exist[offset + i]   = false;
+    if (found_pair_it != sm_device_view.end()) {
+      keys_exist[offset + i] = true;
+      if (found_pair_it->first == insterted_keys[offset + i] and
+          found_pair_it->second == inserted_values[offset + i]) {
         keys_and_values_correct[offset + i] = true;
+      } else {
+        keys_and_values_correct[offset + i] = false;
       }
+    } else {
+      keys_exist[offset + i]              = false;
+      keys_and_values_correct[offset + i] = true;
     }
+  }
 }
 
 TEMPLATE_TEST_CASE_SIG("Shared memory static map",
@@ -272,24 +266,25 @@ TEMPLATE_TEST_CASE_SIG("Shared memory static map",
   constexpr std::size_t elements_in_map = 500;
   constexpr std::size_t map_capacity    = 2 * elements_in_map;
 
-  // one array for all maps, first elements_in_map element belong to map 0, second to map 1 and so on
+  // one array for all maps, first elements_in_map element belong to map 0, second to map 1 and so
+  // on
   std::vector<KeyType> h_keys(number_of_maps * elements_in_map);
   std::vector<ValueType> h_values(number_of_maps * elements_in_map);
   std::vector<cuco::pair_type<KeyType, ValueType>> h_pairs(number_of_maps * elements_in_map);
 
-  // using std::unique_ptr because static_map does not have copy/move constructor/assignment operator yet
+  // using std::unique_ptr because static_map does not have copy/move constructor/assignment
+  // operator yet
   std::vector<std::unique_ptr<MapType>> maps;
 
-  for (std::size_t map_id = 0; map_id < number_of_maps; ++map_id)
-  {
+  for (std::size_t map_id = 0; map_id < number_of_maps; ++map_id) {
     const std::size_t offset = map_id * elements_in_map;
 
-    generate_keys<Dist, KeyType>(h_keys.begin() + offset, h_keys.begin() + offset + elements_in_map);
+    generate_keys<Dist, KeyType>(h_keys.begin() + offset,
+                                 h_keys.begin() + offset + elements_in_map);
 
-    for (std::size_t i = 0; i < elements_in_map; ++i)
-    {
-      KeyType key                             = h_keys[offset + i];
-      ValueType val                           = key < std::numeric_limits<KeyType>::max() ? key + 1 : 0;
+    for (std::size_t i = 0; i < elements_in_map; ++i) {
+      KeyType key                = h_keys[offset + i];
+      ValueType val              = key < std::numeric_limits<KeyType>::max() ? key + 1 : 0;
       h_values[offset + i]       = val;
       h_pairs[offset + i].first  = key;
       h_pairs[offset + i].second = val;
@@ -305,8 +300,7 @@ TEMPLATE_TEST_CASE_SIG("Shared memory static map",
   SECTION("Keys are all found after insertion.")
   {
     std::vector<DeviceViewType> h_device_views;
-    for (std::size_t map_id = 0; map_id < number_of_maps; ++map_id)
-    {
+    for (std::size_t map_id = 0; map_id < number_of_maps; ++map_id) {
       const std::size_t offset = map_id * elements_in_map;
 
       MapType* map = maps[map_id].get();
@@ -318,29 +312,27 @@ TEMPLATE_TEST_CASE_SIG("Shared memory static map",
     thrust::device_vector<bool> d_keys_exist(number_of_maps * elements_in_map);
     thrust::device_vector<bool> d_keys_and_values_correct(number_of_maps * elements_in_map);
 
-    shared_memory_test_kernel<MapType, map_capacity><<<number_of_maps, 64>>>(d_device_views.data().get(),
-                                                                             d_keys.data().get(),
-                                                                             d_values.data().get(),
-                                                                             elements_in_map,
-                                                                             d_keys_exist.data().get(),
-                                                                             d_keys_and_values_correct.data().get());
+    shared_memory_test_kernel<MapType, map_capacity>
+      <<<number_of_maps, 64>>>(d_device_views.data().get(),
+                               d_keys.data().get(),
+                               d_values.data().get(),
+                               elements_in_map,
+                               d_keys_exist.data().get(),
+                               d_keys_and_values_correct.data().get());
 
     REQUIRE(d_keys_exist.size() == d_keys_and_values_correct.size());
-    auto zip = thrust::make_zip_iterator(thrust::make_tuple(d_keys_exist.begin(), d_keys_and_values_correct.begin()));
+    auto zip = thrust::make_zip_iterator(
+      thrust::make_tuple(d_keys_exist.begin(), d_keys_and_values_correct.begin()));
 
-    REQUIRE(all_of(zip,
-                   zip + d_keys_exist.size(),
-                   [] __device__ (auto const& z)
-                   {
-                     return thrust::get<0>(z) and thrust::get<1>(z);
-                   }));
+    REQUIRE(all_of(zip, zip + d_keys_exist.size(), [] __device__(auto const& z) {
+      return thrust::get<0>(z) and thrust::get<1>(z);
+    }));
   }
 
   SECTION("No key is found before insertion.")
   {
     std::vector<DeviceViewType> h_device_views;
-    for (std::size_t map_id = 0; map_id < number_of_maps; ++map_id)
-    {
+    for (std::size_t map_id = 0; map_id < number_of_maps; ++map_id) {
       h_device_views.push_back(maps[map_id].get()->get_device_view());
     }
     thrust::device_vector<DeviceViewType> d_device_views(h_device_views);
@@ -348,18 +340,16 @@ TEMPLATE_TEST_CASE_SIG("Shared memory static map",
     thrust::device_vector<bool> d_keys_exist(number_of_maps * elements_in_map);
     thrust::device_vector<bool> d_keys_and_values_correct(number_of_maps * elements_in_map);
 
-    shared_memory_test_kernel<MapType, map_capacity><<<number_of_maps, 64>>>(d_device_views.data().get(),
-                                                                             d_keys.data().get(),
-                                                                             d_values.data().get(),
-                                                                             elements_in_map,
-                                                                             d_keys_exist.data().get(),
-                                                                             d_keys_and_values_correct.data().get());
+    shared_memory_test_kernel<MapType, map_capacity>
+      <<<number_of_maps, 64>>>(d_device_views.data().get(),
+                               d_keys.data().get(),
+                               d_values.data().get(),
+                               elements_in_map,
+                               d_keys_exist.data().get(),
+                               d_keys_and_values_correct.data().get());
 
-    REQUIRE(none_of(d_keys_exist.begin(),
-                   d_keys_exist.end(),
-                   [] __device__ (const bool key_found)
-                   {
-                     return key_found;
-                   }));
+    REQUIRE(none_of(d_keys_exist.begin(), d_keys_exist.end(), [] __device__(const bool key_found) {
+      return key_found;
+    }));
   }
 }
